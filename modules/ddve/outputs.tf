f01bc4872a6a7959078b4adae94a43d3b85b9bc4
bottkars <karsten.bott@bottnet.de>
2020-07-23 16:37:03 +0200

    output of keys

---
+++ modules/ddve/outputs.tf
@@ -0,0 +1,9 @@
+output "ddve_manager_ssh_public_key" {
+  sensitive = true
+  value     = "${tls_private_key.ddve.public_key_openssh}"
+}
+
+output "ddve_manager_ssh_private_key" {
+  sensitive = true
+  value     = "${tls_private_key.ddve.private_key_pem}"
+}
