e75dc8540ac9861b5d9bd6843dfe402ac27b81f2
bottkars <karsten.bott@bottnet.de>
2020-07-26 09:07:06 +0200

    latest

--- terraforming_ddve/main.tf
+++ terraforming_ddve/main.tf
@@ -8,7 +8,7 @@ provider "azurerm" {
     client_id       = var.client_id
     client_secret   = var.client_secret
     tenant_id       = var.tenant_id
-    environment     = var.cloud_name
+    environment     = azurecloud
 
 
 }
