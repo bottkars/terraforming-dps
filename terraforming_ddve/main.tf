9519ccee1fd063444acf9dd64a2a8bbf4b3ffb0b
bottkars <karsten.bott@bottnet.de>
2020-07-26 08:58:43 +0200

    latest adding pipeline

--- terraforming_ddve/main.tf
+++ terraforming_ddve/main.tf
@@ -4,11 +4,11 @@ provider "azurerm" {
     version = "~>2.0"
     features {}
     # expect to use with env vars, otherwise derive from vars  ...
-    # subscription_id = "${var.subscription_id}"
-    # client_id       = "${var.client_id}"
-    # client_secret   = "${var.client_secret}"
-    # tenant_id       = "${var.tenant_id}"
-    # environment     = "${var.cloud_name}"
+    subscription_id = var.subscription_id
+    client_id       = var.client_id
+    client_secret   = var.client_secret
+    tenant_id       = var.tenant_id
+    environment     = var.cloud_name
 
 
 }
