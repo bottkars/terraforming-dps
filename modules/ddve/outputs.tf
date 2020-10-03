74cb48d87e2635ef7e39acc8df4d84905864fc1b
“bottkars” <Karsten.bott@bottnet.de>
2020-10-03 07:45:14 +0200

    lsatest

--- modules/ddve/outputs.tf
+++ modules/ddve/outputs.tf
@@ -1,9 +1,9 @@
 output "ddve_ssh_public_key" {
   sensitive = true
-  value     = "${tls_private_key.ddve.public_key_openssh}"
+  value     = tls_private_key.ddve.public_key_openssh
 }
 
 output "ddve_ssh_private_key" {
   sensitive = true
-  value     = "${tls_private_key.ddve.private_key_pem}"
+  value     = tls_private_key.ddve.private_key_pem
 }
