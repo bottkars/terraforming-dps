d075acca8b868870bc948a385bd7fc464edb860d
bottkars <karsten.bott@dell.com>
2021-03-15 12:20:51 +0100

    moved to azure

--- modules/linux/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "linux_ssh_public_key" {
-  sensitive = true
-  value     = "${tls_private_key.linux.public_key_openssh}"
-}
-
-output "linux_ssh_private_key" {
-  sensitive = true
-  value     = "${tls_private_key.linux.private_key_pem}"
-}
--- modules/nve/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "nve_ssh_public_key" {
-  sensitive = true
-  value     = "${tls_private_key.nve.public_key_openssh}"
-}
-
-output "nve_ssh_private_key" {
-  sensitive = true
-  value     = "${tls_private_key.nve.private_key_pem}"
-}
