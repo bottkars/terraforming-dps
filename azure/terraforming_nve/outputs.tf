fab15f409508f4a234efd8fac73bc3eb2e85736d
bottkars <karsten.bott@dell.com>
2021-04-06 20:03:12 +0200

    moular tf

--- azure/terraforming_nve/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "nve_ssh_public_key" {
-  sensitive = true
-  value     = "${module.nve.nve_ssh_public_key}"
-}
-
-output "nve_ssh_private_key" {
-  sensitive = true
-  value     = "${module.nve.nve_ssh_private_key}"
-}
