37a7c1343162abd0db057304bfc20180e960dea7
“bottkars” <Karsten.bott@bottnet.de>
2022-01-17 17:47:26 +0100

    ddve outputs

---
+++ terraforming-aws/ddve_output.tf
@@ -0,0 +1,19 @@
+
+output "ddve_private_ip" {
+    value = "${var.create_ddve ? module.ddve[0].ddve_private_ip_address : null }"
+}
+
+
+output "ddve_ssh_private_key" {
+  sensitive = true
+  value     = "${var.create_ddve ? module.ddve[0].ssh_private_key : null}"
+}
+
+output "ddve_ssh_public_key_name" {
+  value = "${var.create_ddve ? module.ddve[0].ssh_public_key_name : null}"
+}
+
+output "ddve_ssh_public_key" {
+  value = "${var.create_ddve ? module.ddve[0].ssh_public_key : null}"
+  sensitive = true
+}
